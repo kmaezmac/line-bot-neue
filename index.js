9f79bdf1b7e84e2779c13a95fa6804f869fca390
Kai Moly <>
2025-06-07 08:43:17 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const line = require('@line/bot-sdk');
+const express = require('express');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+const app = express();
+const client = new line.Client(config);
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result));
+});
+
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: event.message.text // オウム返し
+  });
+}
+
+app.listen(3000, () => {
+  console.log('LINE Bot is running on port 3000');
+});
